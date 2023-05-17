55b0c7e0abed0c6bbc9a8e7f975d305117463040
Dosmukhamed Zhanibekov <dosmukhamed.zhanibekov@obviohealth.com>
2023-05-17 15:04:10 +0800

    adding .env for environmental variable in dev mode

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as process from "process";
 
 @Controller()
 export class AppController {
@@ -7,6 +8,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello() + process.env.DATABASE_URL;
+  }
+
+  @Post('conversation')
+  conversation(@Body() prompt: any): string {
+    return "Result from AI";
   }
 }
